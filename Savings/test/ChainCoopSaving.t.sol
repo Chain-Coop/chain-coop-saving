a0107dbdc6e2398f14dfdcf513a94ebe9159c58c
RonexLemon <ronexondimu@gmail.com>
2024-11-27 13:41:05 +0300

    chore:setup

---
+++ Savings/test/ChainCoopSaving.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.25;
+
+import {Test,console} from "forge-std/Test.sol";
+import {ChainCoopSaving} from "../src/ChainCoopSaving.sol";
+
+
+contract SavingTest is Test{
+    ChainCoopSaving public saving;
+    function setUp()public{
+        saving = new ChainCoopSaving();
+
+    }
+
+
+    
+}
